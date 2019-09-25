9de4e01dbf2a2448dcf47ab3a3a59b94d38df9b6
macstudent <komaldeepkr99@gmail.com>
2019-09-25 19:22:45 -0400

    lambtonstringTools_1

--- src/main/java/com/lambton/C0766343_MidTerm.java
+++ src/main/java/com/lambton/C0766343_MidTerm.java
@@ -11,6 +11,7 @@ public class C0766343_MidTerm
         System.out.println("100000 : "+object.binaryToDecimal("100000"));
         System.out.println("0100110 :"+object.binaryToDecimal("0100110"));
         System.out.println("1000.1 : "+object.binaryToDecimal("1000.1"));
+        System.out.println("0001 : "+object.binaryToDecimal("0001"));
         System.out.println();
 
 
