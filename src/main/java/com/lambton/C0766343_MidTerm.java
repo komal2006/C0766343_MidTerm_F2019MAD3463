a10c8f89b51d24d5d2d8b713f8ba12bd5abbb39c
macstudent <komaldeepkr99@gmail.com>
2019-09-25 19:16:36 -0400

    LambtonStringTools

---
+++ src/main/java/com/lambton/C0766343_MidTerm.java
@@ -0,0 +1,19 @@
+package com.lambton;
+
+public class C0766343_MidTerm
+{
+    public static void main(String[] args)
+    {
+
+        LambtonStringTools object = new LambtonStringTools();
+        System.out.println(" LAMBTON : "+object.reverse("LAMBTON"));
+        System.out.println("1000 : "+object.binaryToDecimal("1000"));
+        System.out.println("100000 : "+object.binaryToDecimal("100000"));
+        System.out.println("0100110 :"+object.binaryToDecimal("0100110"));
+        System.out.println("1000.1 : "+object.binaryToDecimal("1000.1"));
+        System.out.println();
+
+
+    }
+
+}
---
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,39 @@
+package com.lambton;
+
+public class LambtonStringTools
+{
+
+    public String reverse(String s)
+    {
+
+        StringBuilder rev = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            rev.append(s.charAt(i));
+        }
+        return rev.toString();
+    }
+    public int binaryToDecimal(String binary)
+    {
+        int decimal = 0;
+        int binaryMultiplier = 1;
+        for (char c : reverse(binary).toCharArray())
+        {
+            if (c == '1')
+            {
+                decimal += binaryMultiplier;
+            } else if (c == '0')
+            {
+            } else
+                {
+                return -1;
+            }
+            binaryMultiplier *= 2;
+        }
+        return decimal;
+    }
+}
+
+
+
+
