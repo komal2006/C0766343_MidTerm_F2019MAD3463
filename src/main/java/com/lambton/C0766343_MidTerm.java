b4eee536ef748a41a15532ac7e0c3c55946125ed
macstudent <komaldeepkr99@gmail.com>
2019-09-25 19:44:41 -0400

    initials

--- src/main/java/com/lambton/C0766343_MidTerm.java
+++ src/main/java/com/lambton/C0766343_MidTerm.java
@@ -7,12 +7,17 @@ public class C0766343_MidTerm
 
         LambtonStringTools object = new LambtonStringTools();
         System.out.println(" LAMBTON : "+object.reverse("LAMBTON"));
+
+
         System.out.println("1000 : "+object.binaryToDecimal("1000"));
         System.out.println("100000 : "+object.binaryToDecimal("100000"));
         System.out.println("0100110 :"+object.binaryToDecimal("0100110"));
         System.out.println("1000.1 : "+object.binaryToDecimal("1000.1"));
         System.out.println("0001 : "+object.binaryToDecimal("0001"));
         System.out.println();
+
+        System.out.println( object.initaials("jean luc peacrd"));
+
 
 
     }
--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,41 +1,66 @@
 package com.lambton;
+import java.util.*;
+public class LambtonStringTools {
 
-public class LambtonStringTools
-{
-
-    public String reverse(String s)
-    {
+    public String reverse(String s) {
 
         StringBuilder rev = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--)
-        {
+        for (int i = s.length() - 1; i >= 0; i--) {
             rev.append(s.charAt(i));
         }
         return rev.toString();
     }
 
 
-
-    public int binaryToDecimal(String binary)
-    {
+    public int binaryToDecimal(String binary) {
         int decimal = 0;
         int binaryMultiplier = 1;
-        for (char c : reverse(binary).toCharArray())
-        {
-            if (c == '1')
-            {
+        for (char c : reverse(binary).toCharArray()) {
+            if (c == '1') {
                 decimal += binaryMultiplier;
-            } else if (c == '0')
-            {
-            } else
-                {
+            } else if (c == '0') {
+            } else {
                 return -1;
             }
             binaryMultiplier *= 2;
         }
         return decimal;
     }
+
+    public String initaials(String s) {
+        System.out.println("Given String is:" + s);
+        int length = s.length();
+        s = s.trim();
+        String string1 = "";
+        System.out.print("Initials is: ");
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                string1 = string1 + ch;
+            } else {
+                System.out.print(Character.toUpperCase(string1.charAt(0)) + ". ");
+                string1 = "";
+            }
+        }
+        String str2 = "";
+        for (int j = 0; j < string1.length(); j++)
+        {
+            if (j == 0)
+                str2 = str2 + Character.toUpperCase(string1.charAt(0));
+            else
+                str2 = str2 + Character.toLowerCase(string1.charAt(j));
+        }
+        return str2;
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
